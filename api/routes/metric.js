e305e76c13cc191ba62a6421193b78e9b7d9c3f8
Khaled Ghamgui <gham.khaled@gmail.com>
2022-01-17 21:40:03 +0100

    Fix Prometheus

--- api/routes/metric.js
+++ api/routes/metric.js
@@ -2,7 +2,7 @@ const client = require('prom-client');
 const router = require("express").Router();
 
 const collectDefaultMetrics = client.collectDefaultMetrics;
-collectDefaultMetrics({ timeout: 5000 });
+collectDefaultMetrics({timeout: 5000});
 
 const counter = new client.Counter({
     name: 'node_request_operations_total',
@@ -21,16 +21,19 @@ router.post("/", async (req, res) => {
     //Simulate a sleep
     var start = new Date()
     var simulateTime = 1000
-    setTimeout(function(argument) {
+    await setTimeout(function (argument) {
         // execution time simulated with setTimeout function
         var end = new Date() - start
         histogram.observe(end / 1000); //convert to seconds
     }, simulateTime)
     counter.inc();
+    res.send('Done')
 });
-router.get('/', (req, res) => {
+router.get('/', async(req, res) =>  {
     res.set('Content-Type', client.register.contentType)
-    res.end(client.register.metrics())
+    const metrics = await client.register.metrics()
+    res.end(metrics)
 });
 
 
+module.exports = router;
