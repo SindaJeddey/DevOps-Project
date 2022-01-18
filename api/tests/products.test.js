f71ccd567c2418ea2101aa1cc41f233ff4ba21f6
Khaled Ghamgui <gham.khaled@gmail.com>
2022-01-18 09:15:21 +0100

    Test Env AWS Credentials

--- api/tests/products.test.js
+++ api/tests/products.test.js
@@ -4,7 +4,8 @@ const app = express(); //an instance of an express app, a 'fake' express app
 const dotenv = require("dotenv");
 dotenv.config();
 const AWS = require('aws-sdk-mock');
-
+console.log(process.env.AWS_ACCESS_KEY_ID)
+console.log(process.env.AWS_SECRET_ACCESS_KEY)
 AWS.mock('DynamoDB', 'scan', function (params, callback) {
     callback(null, {
         'Items': [
